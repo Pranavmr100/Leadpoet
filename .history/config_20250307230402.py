9ef0225e10383ca1c14a98564d71c6e829f6825d
Pranavmr100 <pmr2164@columbia.edu>
2025-03-16 20:18:12 -0400

    Delete .history directory

--- .history/config_20250307230402.py
+++
@@ -1,8 +0,0 @@
-# config.py
-
-# API settings (where our server runs)
-API_HOST = "0.0.0.0"  # Means "accessible from anywhere"
-API_PORT = 8000       # The port number (like a door for connections)
-
-# Encryption key (replace this with a secure key later)
-ENCRYPTION_KEY = b"mysecretkey1234567890abcdefghij"  # Must be 32 bytes
